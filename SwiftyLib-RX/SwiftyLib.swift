63289215739f2b3ba3c8a4f3e1063ca8a19c4a0f
Rex <rex.hsu@alivecor.com>
2022-03-29 15:18:46 -0700

    Added test cases

---
+++ SwiftyLib-RX/SwiftyLib.swift
@@ -0,0 +1,23 @@
+//
+//  SwiftyLib.swift
+//  SwiftyLib-RX
+//
+//  Created by Rex Hsu on 3/29/22.
+//
+
+import Foundation
+
+
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
