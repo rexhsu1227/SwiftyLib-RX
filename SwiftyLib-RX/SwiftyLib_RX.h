c08a068427e156473b2b67ed2553196ad5430e81
Rex <rex.hsu@alivecor.com>
2022-03-29 15:10:22 -0700

    Initial project setup

---
+++ SwiftyLib-RX/SwiftyLib_RX.h
@@ -0,0 +1,18 @@
+//
+//  SwiftyLib_RX.h
+//  SwiftyLib-RX
+//
+//  Created by Rex Hsu on 3/29/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyLib_RX.
+FOUNDATION_EXPORT double SwiftyLib_RXVersionNumber;
+
+//! Project version string for SwiftyLib_RX.
+FOUNDATION_EXPORT const unsigned char SwiftyLib_RXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyLib_RX/PublicHeader.h>
+
+
